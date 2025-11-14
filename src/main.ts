6d01cf108a331c5de1612e533bea720491e9e3bc
Trần Đình Nam <trandinhnamuet@gmail.com>
2025-11-14 10:27:47 +0700

    tang gioi han payload body size getfly x10 lan

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+  });
+  
+  // Tăng giới hạn body size để xử lý payload lớn từ GetflyCRM
+  app.use('/getfly/callback', (req, res, next) => {
+    // Tăng limit lên 1MB cho callback GetflyCRM
+    const bodyParser = require('body-parser');
+    const jsonParser = bodyParser.json({ limit: '1mb' });
+    const urlencodedParser = bodyParser.urlencoded({ limit: '1mb', extended: true });
+    
+    if (req.headers['content-type'] && req.headers['content-type'].includes('application/json')) {
+      jsonParser(req, res, next);
+    } else if (req.headers['content-type'] && req.headers['content-type'].includes('application/x-www-form-urlencoded')) {
+      urlencodedParser(req, res, next);
+    } else {
+      next();
+    }
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
